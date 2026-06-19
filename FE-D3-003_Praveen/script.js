12d09bd4b4344f2ae7af9a2b27b8110320876b0f
praveenpb-git-20 <praveenpb1120@gmail.com>
2026-06-19 17:33:23 +0530

    Add files via upload

---
+++ FE-D3-003_Praveen/script.js
@@ -0,0 +1,29 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(count < target){
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCounter,20);
+        }
+        else{
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
